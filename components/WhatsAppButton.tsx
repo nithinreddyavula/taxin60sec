99097b86bb0f195a74d2b05b0bee6451634c1ac8
Nithin Reddy <nithinreddy34261@gmail.com>
2026-05-16 22:20:48 +0530

    added whatsapp floating button

---
+++ components/WhatsAppButton.tsx
@@ -0,0 +1,13 @@
+export default function WhatsAppButton() {
+
+  return (
+    <a
+      href="https://wa.me/917013734079"
+      target="_blank"
+      rel="noopener noreferrer"
+      className="fixed bottom-6 right-6 bg-green-500 text-white px-6 py-4 rounded-full shadow-lg hover:scale-105 transition z-50"
+    >
+      WhatsApp
+    </a>
+  );
+}
