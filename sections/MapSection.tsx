3aa1430ffe20a4429f9a2492fddbbe95caa93b62
Nithin Reddy <nithinreddy34261@gmail.com>
2026-05-17 12:15:31 +0530

    fixed google maps iframe jsx errors

--- sections/MapSection.tsx
+++ sections/MapSection.tsx
@@ -16,12 +16,15 @@ export default function MapSection() {
         <div className="rounded-[30px] overflow-hidden shadow-lg">
 
           <iframe
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d289.0476787380129!2d77.57696482946456!3d12.911159472607718!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bae1500604f5e69%3a0xa1d4fcab8c3f715a!2stech%20bite%e2%80%99s%20cafe!5e0!3m2!1sen!2sin!4v1778999692340!5m2!1sen!2sin" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d289.0476787380129!2d77.57696482946456!3d12.911159472607718!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bae1500604f5e69%3a0xa1d4fcab8c3f715a!2stech%20bite%E2%80%99s%20cafe!5e0!3m2!1sen!2sin!4v1778999692340!5m2!1sen!2sin"
             width="100%"
             height="500"
+            style={{ border: 0 }}
+            allowFullScreen
             loading="lazy"
-            className="border-0"
-          />
+            referrerPolicy="no-referrer-when-downgrade"
+            className="rounded-[30px]"
+          ></iframe>
 
         </div>
 
