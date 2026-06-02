e1a5d43e9512a6ce213a27b830c798205b428df5
Nithin Reddy <nithinreddy34261@gmail.com>
2026-06-02 16:15:22 +0530

    fix CORS

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,22 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+
+  images: {
+
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
+    ],
+
+  },
+
+  allowedDevOrigins: [
+    "192.168.31.20",
+  ],
+
 };
 
 export default nextConfig;
